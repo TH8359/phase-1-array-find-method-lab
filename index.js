e123c86da4690321b92eacf79eb3c324b109ea1b
Tyler Hogue <hogue73@embarqmail.com>
2024-12-18 21:27:00 -0500

    passed tests

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(games){
+let game = 0
+while (game < games.length){
+    if (games[game].result === "W"){
+        return games[game].year
+    }
+    game++
+}
+}
+
+
+Array.prototype.find(superbowlWin)
